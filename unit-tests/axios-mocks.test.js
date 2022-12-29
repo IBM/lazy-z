8017b0fad6a39969d439222f6f2b0f28ec40005e
jvallexm <jvallexm@gmail.com>
2022-12-29 16:30:24 -0500

    fix: axios, revision contenxt

--- unit-tests/axios-mocks.test.js
+++ unit-tests/axios-mocks.test.js
@@ -1,16 +1,16 @@
 const { assert } = require("chai");
-const mockAxios = require("../lib/axios-mocks");
+const { initMockAxios } = require("../lib/axios-mocks");
 
 describe("axios", () => {
   describe("main function", () => {
     it("should return a promise when called with params on resolve", () => {
-      let { axios } = mockAxios({ data: true });
+      let { axios } = initMockAxios({ data: true });
       return axios({ params: "yes" }).then((data) => {
         assert.isTrue(data.data.data, "it should be true");
       });
     });
     it("should reject when error is true", () => {
-      let { axios } = mockAxios({ data: true }, true);
+      let { axios } = initMockAxios({ data: true }, true);
       return axios({ params: "yes" }).catch((data) => {
         assert.isTrue(data.data, "it should be true");
       });
@@ -18,7 +18,7 @@ describe("axios", () => {
   });
   describe("get function", () => {
     it("should return a promise when called with params on resolve", () => {
-      let { axios } = mockAxios({ data: true });
+      let { axios } = initMockAxios({ data: true });
       return axios.get({ params: "yes" }).then((data) => {
         assert.isTrue(data.data.data, "it should be true");
       });
